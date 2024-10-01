b29fef411fa31d423ceb37fcfd66129bb7121b8c
Alex Gutierrez <agutierrezlavin@gmail.com>
2024-10-01 17:51:28 +0200

    Add auth to api call

--- src/UrlEncoder/Url/Infrastructure/supabase/SupabaseRepository.ts
+++ src/UrlEncoder/Url/Infrastructure/supabase/SupabaseRepository.ts
@@ -7,6 +7,8 @@ export class SupabaseRepository implements UrlRepository {
   SUPABASE_URL = process.env.SUPABASE_URL ?? ''
   SUPABASE_KEY = process.env.SUPABASE_KEY ?? ''
   SUPABASE_URLS_TABLE = process.env.SUPABASE_URLS_TABLE ?? ''
+  SUPABASE_USER_EMAIL = process.env.SUPABASE_USER_EMAIL ?? ''
+  SUPABASE_USER_PASSWORD = process.env.SUPABASE_USER_PASSWORD ?? ''
 
   supabaseClient
 
@@ -16,7 +18,9 @@ export class SupabaseRepository implements UrlRepository {
 
   async saveUrl(encodedDecoded: EncodedDecoded): Promise<void> {
     try {
-      const { error } = await this.supabaseClient.from(this.SUPABASE_URLS_TABLE).insert({
+      await this.#authUser()
+
+      await this.supabaseClient.from(this.SUPABASE_URLS_TABLE).insert({
         encoded_url: encodedDecoded.encodedUrl.get(),
         decoded_url: encodedDecoded.decodedUrl.get()
       })
@@ -27,13 +31,28 @@ export class SupabaseRepository implements UrlRepository {
 
   async getDecodedUrl(encodedUrl: string): Promise<DecodedUrl | undefined> {
     try {
+      await this.#authUser()
+
       const { data } = await this.supabaseClient.from(this.SUPABASE_URLS_TABLE).select().eq('encoded_url', encodedUrl)
+
       if (!data) {
         return
       }
 
       return new DecodedUrl(data[0].decoded_url ?? '')
     } catch (error) {
+      throw new Error()
+    }
+  }
+
+  async #authUser() {
+    try {
+      await this.supabaseClient.auth.signInWithPassword({
+        email: this.SUPABASE_USER_EMAIL,
+        password: this.SUPABASE_USER_PASSWORD
+      })
+    } catch (e) {
+      console.error({ error: e })
       throw new Error()
     }
   }
